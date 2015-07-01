bc33df753ffa2eb8c1d5629e5fc4736037454216
eondcom <eond@eond.com>
2015-07-01 19:13:49 +0900

    upload

---
+++ modules/planet.mobile.php
@@ -0,0 +1,16 @@
+<?
+	/**
+	* @class planetMobile
+	* @author 이온디 (http://eond.com, eond@eond.com)
+	* @brief planet 모듈의 Mobile class
+	**/
+
+	class planetMobile extends planet {
+
+		fuction procMobile(&$oMobile){
+			$prev_date = Context::get('prev_date');
+			if($prev_date) $
+		}
+
+	}
+?>
